af36eb5e57c1751b1ecfb8c007a66b32a246675f
William <williamnharvey@gmail.com>
2018-02-25 00:19:02 -0500

    just awful

---
+++ williamnharvey.bat
@@ -0,0 +1,37 @@
+@echo off
+cls
+title WilliamNHarvey
+set NUM=0123456789ABCDEF
+SET /A first=%RANDOM% * 16 / 32768 + 1
+SET /A second=%RANDOM% * 16 / 32768 + 1
+for /l %%i in (%first%,1,%first%) do (
+  call set bit=%%NUM:~%%i,1%%	
+)
+for /l %%i in (%second%,1,%second%) do (
+  call set bat=%%NUM:~%%i,1%%	
+)
+for %%x in (%bit%) do ( 
+    for %%y in (%bat%) do (
+        color %%x%%y
+    )
+)
+cd %HOMEPATH%
+echo.
+:cmd
+set /p "cmd=%cd%>"
+%cmd%
+echo.
+SET /A first=%RANDOM% * 16 / 32768 + 1
+SET /A second=%RANDOM% * 16 / 32768 + 1
+for /l %%i in (%first%,1,%first%) do (
+  call set bit=%%NUM:~%%i,1%%	
+)
+for /l %%i in (%second%,1,%second%) do (
+  call set bat=%%NUM:~%%i,1%%	
+)
+for %%x in (%bit%) do ( 
+    for %%y in (%bat%) do (
+        color %%x%%y
+    )
+)
+goto cmd
